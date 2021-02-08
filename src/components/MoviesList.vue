61b180e9c568fef6fec446e2d823cf26355b4be5
0xLDev <wodinxe@yandex.com>
2021-02-08 15:50:39 +0700

    moviesapp

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,110 @@
+<template>
+  <BContainer>
+    <h3 class="list-title">{{ listTitle }}</h3>
+    <BRow>
+      <template v-if="isExist">
+        <BCol cols="3" v-for="(movie, key) in list" :key="key">
+          <MovieItem
+            :movie="movie"
+            @mouseover.native="onMouseOver(movie.Poster)"
+            @removeItem="onRemoveItem"
+            @showModal="onShowMovieInfo"
+          />
+        </BCol>
+      </template>
+      <template v-else>
+        <div>Пустой список</div>
+      </template>
+    </BRow>
+    <BModal
+      body-class="movie-modal-body"
+      :id="movieInfoModalID"
+      size="xl"
+      hide-footer
+      hide-header
+    >
+      <ModalInfoModalContent
+        :movie="selectedMovie"
+        @closeModal="onCloseModal"
+      />
+    </BModal>
+  </BContainer>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import MovieItem from "./MovieItem";
+import ModalInfoModalContent from "./MovieInfoModalContent";
+
+export default {
+  name: "MoviesList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    movieInfoModalID: "movie-info",
+    selectedMovieID: ""
+  }),
+  components: {
+    MovieItem,
+    ModalInfoModalContent
+  },
+  computed: {
+    ...mapGetters("movies", ["isSearch"]),
+    isExist() {
+      return Boolean(Object.keys(this.list).length);
+    },
+    listTitle() {
+      return this.isSearch ? "Результат поиска" : "IMDB Top 250";
+    },
+    selectedMovie() {
+      return this.selectedMovieID ? this.list[this.selectedMovieID] : null;
+    }
+  },
+  methods: {
+    ...mapActions("movies", ["removeMovie"]),
+    ...mapActions(["showNotify"]),
+    onMouseOver(poster) {
+      this.$emit("changePoster", poster);
+    },
+    async onRemoveItem({ id, title }) {
+      const isConfirmed = await this.$bvModal.msgBoxConfirm(
+        `Вы действительно хотите удалить ${title}?`
+      );
+      if (isConfirmed) {
+        this.removeMovie(id);
+        this.showNotify({
+          msg: "Фильм успешно удален",
+          variant: "success",
+          title: "Успешно"
+        });
+      }
+    },
+    onShowMovieInfo(id) {
+      this.selectedMovieID = id;
+      this.$bvModal.show(this.movieInfoModalID);
+    },
+    onCloseModal() {
+      this.selectedMovieID = null;
+      this.$bvModal.hide(this.movieInfoModalID);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.list-title {
+  font-size: 50px;
+  color: #fff;
+  margin-bottom: 30px;
+}
+</style>
+
+<style>
+.movie-modal-body {
+  padding: 0 !important;
+}
+</style>
